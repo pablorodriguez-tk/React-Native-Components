59830cea011255fff212ca1767444cc385b902ba
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-10 15:44:31 -0300

    add Modal Component

---
+++ src/screens/ModalScreen.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {Button, Modal, Text, View} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
+
+export const ModalScreen = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <View style={styles.globalMargin}>
+      <HeaderTitle title="Modal screen" />
+      <Button
+        title="Abrir Modal"
+        onPress={() => {
+          setIsVisible(true);
+        }}
+      />
+      <Modal animationType="fade" visible={isVisible} transparent={true}>
+        {/* Background negro */}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {/* Contenido del modal */}
+          <View
+            style={{
+              backgroundColor: 'white',
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowOffset: {width: 0, height: 10},
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 5,
+            }}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Modal</Text>
+            <Text style={{fontSize: 16, fontWeight: '300', marginBottom: 20}}>
+              Cuerpo del modal
+            </Text>
+            <Button title="Salir" onPress={() => setIsVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
