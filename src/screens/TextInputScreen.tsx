e705939b44d3506cf1e131f2e8673389be359275
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-10 01:30:45 -0300

    Add inputs component

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,13 +1,88 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  Text,
+} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
+import {useForm} from '../hooks/useForm';
 import {styles} from '../theme/appTheme';
+import {CustomSwitch} from '../components/CustomSwitch';
 
 export const TextInputScreen = () => {
+  const {form, isSubscribed, onChange} = useForm({
+    name: '',
+    email: '',
+    phone: '',
+    isSubscribed: false,
+  });
+
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="TextInputs" />
-      <TextInput />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.globalMargin}>
+            <HeaderTitle title="TextInputs" />
+            <TextInput
+              style={stylesScreen.input}
+              placeholder="Ingrese su nombre"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange(value, 'name')}
+            />
+            <TextInput
+              style={stylesScreen.input}
+              placeholder="Ingrese su email"
+              autoCorrect={false}
+              onChangeText={value => onChange(value, 'email')}
+              keyboardType="email-address"
+              keyboardAppearance="dark"
+            />
+            <TextInput
+              style={stylesScreen.input}
+              placeholder="Ingrese su telefono"
+              onChangeText={value => onChange(value, 'phone')}
+              keyboardType="phone-pad"
+            />
+            {/* Ejemplo */}
+
+            <View style={stylesScreen.switchRow}>
+              <Text style={stylesScreen.switchText}>Suscribirse:</Text>
+              <CustomSwitch
+                isOn={isSubscribed}
+                onChange={value => onChange(value, 'isSubscribed')}
+              />
+            </View>
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <View style={{height: 100}} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
+
+const stylesScreen = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.3)',
+    height: 50,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  switchText: {fontSize: 25},
+});
